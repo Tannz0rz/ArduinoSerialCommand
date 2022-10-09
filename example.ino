2a1cc245bbd42c8649c7c4d16c95c6bfd44c2586
Casey Sanchez <nabbz0rz@gmail.com>
2022-10-08 21:53:08 -0400

    Initial commit

---
+++ example.ino
@@ -0,0 +1,114 @@
+#include "serial_command_server.hpp"
+
+enum : uint8_t
+{
+    ADD_COMMAND = 0,
+    SUB_COMMAND = 1,
+    MUL_COMMAND = 2,
+    DIV_COMMAND = 3,
+};
+
+SerialCommandServer<4> serial_command_server;
+
+// Command identifier: ADD_COMMAND (3)
+// Request: a -> uint32_t, b -> uint32_t
+// Response: sum -> uint32_t
+uint8_t add_command(uint8_t const &request_size, uint8_t const *request_buffer, uint8_t &response_size, uint8_t *response_buffer) 
+{
+    if (request_size == sizeof(uint32_t) + sizeof(uint32_t)) {
+        uint32_t a;
+        uint32_t b;
+        
+        memcpy(&a, &request_buffer[0], sizeof(uint32_t));
+        memcpy(&b, &request_buffer[sizeof(uint32_t)], sizeof(uint32_t));
+      
+        uint32_t sum = a + b;
+      
+        memcpy(&response_buffer[0], &sum, sizeof(uint32_t));      
+      
+        return SerialCommandStatus::SUCCESS;
+    }
+  
+    return SerialCommandStatus::FAILURE;
+}
+
+// Command identifier: SUB_COMMAND (3)
+// Request: a -> uint32_t, b -> uint32_t
+// Response: difference -> uint32_t
+uint8_t sub_command(uint8_t const &request_size, uint8_t const *request_buffer, uint8_t &response_size, uint8_t *response_buffer) 
+{
+    if (request_size == sizeof(uint32_t) + sizeof(uint32_t)) {
+        uint32_t a;
+        uint32_t b;
+        
+        memcpy(&a, &request_buffer[0], sizeof(uint32_t));
+        memcpy(&b, &request_buffer[sizeof(uint32_t)], sizeof(uint32_t));
+      
+        uint32_t difference = a - b;
+      
+        memcpy(&response_buffer[0], &difference, sizeof(uint32_t));      
+      
+        return SerialCommandStatus::SUCCESS;
+    }
+  
+    return SerialCommandStatus::FAILURE;
+}
+
+// Command identifier: MUL_COMMAND (3)
+// Request: a -> uint32_t, b -> uint32_t
+// Response: product -> uint32_t
+uint8_t mul_command(uint8_t const &request_size, uint8_t const *request_buffer, uint8_t &response_size, uint8_t *response_buffer) 
+{
+    if (request_size == sizeof(uint32_t) + sizeof(uint32_t)) {
+        uint32_t a;
+        uint32_t b;
+        
+        memcpy(&a, &request_buffer[0], sizeof(uint32_t));
+        memcpy(&b, &request_buffer[sizeof(uint32_t)], sizeof(uint32_t));
+      
+        uint32_t product = a * b;
+      
+        memcpy(&response_buffer[0], &product, sizeof(uint32_t));      
+      
+        return SerialCommandStatus::SUCCESS;
+    }
+  
+    return SerialCommandStatus::FAILURE;
+}
+
+// Command identifier: DIV_COMMAND (3)
+// Request: a -> uint32_t, b -> uint32_t
+// Response: quotient -> uint32_t
+uint8_t div_command(uint8_t const &request_size, uint8_t const *request_buffer, uint8_t &response_size, uint8_t *response_buffer) 
+{
+    if (request_size == sizeof(uint32_t) + sizeof(uint32_t)) {
+        uint32_t a;
+        uint32_t b;
+        
+        memcpy(&a, &request_buffer[0], sizeof(uint32_t));
+        memcpy(&b, &request_buffer[sizeof(uint32_t)], sizeof(uint32_t));
+      
+        uint32_t quotient = a / b;
+      
+        memcpy(&response_buffer[0], &quotient, sizeof(uint32_t));      
+      
+        return SerialCommandStatus::SUCCESS;
+    }
+  
+    return SerialCommandStatus::FAILURE;
+}
+
+void setup()
+{
+    serial_command_server.setup(115200);
+    
+    serial_command_server.registerCommand(ADD_COMMAND, &add_command);
+    serial_command_server.registerCommand(SUB_COMMAND, &sub_command);
+    serial_command_server.registerCommand(MUL_COMMAND, &mul_command); 
+    serial_command_server.registerCommand(DIV_COMMAND, &div_command); 
+}
+
+void loop()
+{
+    serial_command_server.listen();  
+}
---
+++ serial_command_server.hpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2022 Casey Sanchez
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+enum SerialCommandStatus : uint8_t
+{
+    FAILURE = 0,
+    SUCCESS = 1
+};
+
+struct SerialCommandRequest
+{
+    uint8_t command;
+    uint8_t size;
+    uint8_t buffer[255];
+};
+
+struct SerialCommandResponse
+{
+    uint8_t status;
+    uint8_t size;
+    uint8_t buffer[255];
+};
+
+template <size_t MAX_COMMANDS>
+class SerialCommandServer
+{
+    struct SerialCommand
+    {
+        uint8_t command;
+        uint8_t (*callback)(uint8_t const &, uint8_t const *, uint8_t &, uint8_t *);
+    };
+  
+    SerialCommand m_serial_commands[MAX_COMMANDS];
+  
+    size_t m_num_commands;
+  
+public:
+    SerialCommandServer() :
+        m_num_commands { 0 }
+    {
+    }
+
+    void setup(uint32_t const &baud_rate)
+    {
+        Serial.begin(baud_rate);
+        
+        while (!Serial);
+    }
+
+    bool registerCommand(uint8_t const &command, uint8_t (*callback)(uint8_t const &, uint8_t const *, uint8_t &, uint8_t *))
+    {
+        if (m_num_commands >= MAX_COMMANDS || callback == nullptr) {
+            return false;
+        }
+        else {
+            size_t const index = m_num_commands++;
+          
+            m_serial_commands[index].command = command;
+            m_serial_commands[index].callback = callback;
+          
+            return true;
+        }
+    }
+    
+    void listen()
+    {      
+        if (Serial.available()) {
+            SerialCommandRequest request;
+            SerialCommandResponse response;
+        
+            Serial.readBytes(&request.command, sizeof(uint8_t));  
+            Serial.readBytes(&request.size, sizeof(uint8_t));
+        
+            if (request.size > 0) {
+                Serial.readBytes(&request.buffer[0], request.size);
+            }
+
+            response.status = SerialCommandStatus::FAILURE;
+            response.size = 0;
+
+            for (size_t index = 0; index < m_num_commands; ++index) {
+                if (m_serial_commands[index].command == request.command) {
+                    response.status = m_serial_commands[index].callback(request.size, request.buffer, response.size, response.buffer);
+            
+                    break;
+                }
+            }
+
+            if (response.status == SerialCommandStatus::FAILURE) {
+                response.size = 0;
+            }
+
+            Serial.write(&response.status, sizeof(uint8_t));
+            Serial.write(&response.size, sizeof(uint8_t));
+        
+            if (response.size > 0) {
+                Serial.write(&response.buffer[0], response.size);
+            }
+        }
+    }
+};
